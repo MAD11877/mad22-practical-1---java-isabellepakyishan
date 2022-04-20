9c70a7e17ed70229e524b11e39033fb68adf0bae
isabelle_pak <s10222456@connect.np.edu.sg>
2022-04-20 19:32:54 +0800

    Removed unnecessary spaces for Exercise 1 Qn 3

--- Question4.java
+++ Question4.java
@@ -22,7 +22,7 @@ public class Question4
 
     for (int i = randomInt; i > 0; i--){
       for (int j = 0; j < i; j++){
-        System.out.print("* ");
+        System.out.print("*");
       }
       System.out.println();
     }
